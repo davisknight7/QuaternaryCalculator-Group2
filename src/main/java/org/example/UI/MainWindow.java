1391d82625335d1b414e62c95499ee9063982016
davisknight7 <davis.knight@bsu.edu>
2023-09-06 12:56:24 -0400

    Added display window with formatting, and 4 basic operation buttons.

--- src/main/java/org/example/UI/MainWindow.java
+++ src/main/java/org/example/UI/MainWindow.java
@@ -2,11 +2,16 @@ package org.example.UI;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 import java.util.concurrent.ExecutorService;
@@ -14,7 +19,11 @@ import java.util.concurrent.Executors;
 
 
 public class MainWindow extends Application {
-    private final Label display = new Label("8");
+    private final Button addButton = new Button("+");
+    private final Button subtractButton = new Button("-");
+    private final Button multiplyButton = new Button("*");
+    private final Button divideButton = new Button("/");
+
     private final ExecutorService executor = Executors.newCachedThreadPool();
 
     @Override
@@ -34,10 +43,37 @@ public class MainWindow extends Application {
     }
 
     private Parent createCalculatorView() {
-        VBox vBox = new VBox();
-        vBox.getChildren().addAll(
-            display
-        );
-        return vBox;
+        VBox totalView = new VBox();
+        totalView.setPrefWidth(400);
+        totalView.setPrefHeight(600);
+
+        Label label = setupDisplay(totalView);
+        HBox rowOneOperations = setupRowOneOperations(totalView);
+
+        totalView.getChildren().addAll(label, rowOneOperations);
+
+        return totalView;
+    }
+
+    private HBox setupRowOneOperations(VBox totalView) {
+        HBox row = new HBox();
+
+        addButton.setPrefSize(totalView.getPrefWidth()/4, totalView.getPrefHeight()/4);
+        subtractButton.setPrefSize(totalView.getPrefWidth()/4, totalView.getPrefHeight()/4);
+        multiplyButton.setPrefSize(totalView.getPrefWidth()/4, totalView.getPrefHeight()/4);
+        divideButton.setPrefSize(totalView.getPrefWidth()/4, totalView.getPrefHeight()/4);
+        row.getChildren().addAll(addButton, subtractButton, multiplyButton, divideButton);
+
+        return row;
+    }
+
+    private Label setupDisplay(VBox totalView) {
+        Label display = new Label("8");
+        display.setPrefWidth(totalView.getPrefWidth());
+        display.setPrefHeight(totalView.getPrefHeight()/4);
+
+        display.setAlignment(Pos.CENTER_RIGHT);
+        display.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 40));
+        return display;
     }
 }
