a4202804f6f77a8605e2026cf460baf106101913
Timothy Nicholas <tsnicholas@bsu.edu>
2023-09-06 15:20:10 -0400

    Adds new buttons to user interface

--- src/main/java/org/example/UI/MainWindow.java
+++ src/main/java/org/example/UI/MainWindow.java
@@ -28,6 +28,10 @@ public class MainWindow extends Application {
     private final Button subtractButton = new Button("-");
     private final Button multiplyButton = new Button("*");
     private final Button divideButton = new Button("/");
+    private final Button squareButton = new Button("^");
+    private final Button rootButton = new Button("sqrt");
+    private final Button clearButton = new Button("C");
+    private final Button convertButton = new Button("Convert");
     private final Button computeButton = new Button("=");
 
     private final ExecutorService executor = Executors.newCachedThreadPool();
@@ -35,17 +39,21 @@ public class MainWindow extends Application {
     @Override
     public void init() throws Exception {
         super.init();
+        int numOfButtonsPerRow = 4;
         for(int i = 0; i < 4; i++) {
             numberButton[i] = new Button(String.valueOf(i));
-            numberButton[i].setPrefSize((double)WINDOW_WIDTH / 3, (double)LABEL_HEIGHT / 3);
+            numberButton[i].setPrefSize((double)WINDOW_WIDTH / numOfButtonsPerRow, (double)LABEL_HEIGHT / numOfButtonsPerRow);
         }
-        addButton.setPrefSize((double)WINDOW_WIDTH / 3, (double)LABEL_HEIGHT / 3);
-        subtractButton.setPrefSize((double)WINDOW_WIDTH / 3, (double)LABEL_HEIGHT / 3);
-        multiplyButton.setPrefSize((double)WINDOW_WIDTH / 3, (double)LABEL_HEIGHT / 3);
-        divideButton.setPrefSize((double)WINDOW_WIDTH / 3, (double)LABEL_HEIGHT / 3);
-        computeButton.setPrefSize((double)WINDOW_WIDTH / 3, (double)LABEL_HEIGHT / 3);
+        addButton.setPrefSize((double)WINDOW_WIDTH / numOfButtonsPerRow, (double)LABEL_HEIGHT / numOfButtonsPerRow);
+        subtractButton.setPrefSize((double)WINDOW_WIDTH / numOfButtonsPerRow, (double)LABEL_HEIGHT / numOfButtonsPerRow);
+        multiplyButton.setPrefSize((double)WINDOW_WIDTH / numOfButtonsPerRow, (double)LABEL_HEIGHT / numOfButtonsPerRow);
+        divideButton.setPrefSize((double)WINDOW_WIDTH / numOfButtonsPerRow, (double)LABEL_HEIGHT / numOfButtonsPerRow);
+        computeButton.setPrefSize((double)WINDOW_WIDTH / numOfButtonsPerRow, (double)LABEL_HEIGHT / numOfButtonsPerRow);
+        squareButton.setPrefSize((double)WINDOW_WIDTH / numOfButtonsPerRow, (double)LABEL_HEIGHT / numOfButtonsPerRow);
+        rootButton.setPrefSize((double)WINDOW_WIDTH / numOfButtonsPerRow, (double)LABEL_HEIGHT / numOfButtonsPerRow);
+        clearButton.setPrefSize((double)WINDOW_WIDTH / numOfButtonsPerRow, (double)LABEL_HEIGHT / numOfButtonsPerRow);
+        convertButton.setPrefSize(WINDOW_WIDTH, (double)LABEL_HEIGHT / numOfButtonsPerRow);
     }
-
 
     @Override
     public void start(Stage primaryStage) {
@@ -88,20 +96,21 @@ public class MainWindow extends Application {
         GridPane gridPane = new GridPane();
         Button[][] layout = {
                 new Button[] {
-                    numberButton[3], numberButton[2], multiplyButton
+                    numberButton[3], numberButton[2], multiplyButton, squareButton
                 },
                 new Button[] {
-                    numberButton[1], numberButton[0], divideButton
+                    numberButton[1], numberButton[0], divideButton, rootButton
                 },
                 new Button[] {
-                    addButton, subtractButton, computeButton
+                    addButton, subtractButton, computeButton, clearButton
                 }
         };
-        for(int j = 0; j < 3; j++) {
+        for(int j = 0; j < 4; j++) {
             for(int i = 0; i < 3; i++) {
                 gridPane.add(layout[i][j], j, i);
             }
         }
+        gridPane.add(convertButton, 0, 4, 4, 1);
         return gridPane;
     }
 }
