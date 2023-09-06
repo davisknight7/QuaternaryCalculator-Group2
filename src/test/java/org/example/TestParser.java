f3597e50fba63a10641c955a26ccf35d46f59b0a
evyncking <ecking@bsu.edu>
2023-09-06 16:42:02 -0400

    Add test cases for parser

--- src/test/java/org/example/TestParser.java
+++ src/test/java/org/example/TestParser.java
@@ -13,4 +13,25 @@ public class TestParser {
         String[] expected = new String[]{"01", "+", "10"};
         assertArrayEquals(expected, parser.parse("01+10"));
     }
+
+    @Test
+    void testParseSubtraction(){
+        Parser parser = new Parser();
+        String[] expected = new String[]{"03", "-", "30"};
+        assertArrayEquals(expected, parser.parse("03-30"));
+    }
+
+    @Test
+    void testParseMultiplication(){
+        Parser parser = new Parser();
+        String[] expected = new String[]{"13", "*", "31"};
+        assertArrayEquals(expected, parser.parse("13*31"));
+    }
+
+    @Test
+    void testParseDivision(){
+        Parser parser = new Parser();
+        String[] expected = new String[]{"01", "/", "10"};
+        assertArrayEquals(expected, parser.parse("01/10"));
+    }
 }
