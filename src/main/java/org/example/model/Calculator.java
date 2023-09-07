ef1e1c8853fc6e1c1d8455720a08601d0afe6127
PandorasGit <wendallar125@gmail.com>
2023-09-07 15:56:36 -0400

    Remove enhanced switch for legacy support and add idea folder to git ignore

--- src/main/java/org/example/model/Calculator.java
+++ src/main/java/org/example/model/Calculator.java
@@ -4,31 +4,31 @@ public class  Calculator {
     public String calculate(int first, int second, String operation){
         Converter converter = new Converter();
         switch (operation) {
-            case "+" -> {
+            case "+":
                 return converter.convertToQuaternary(first + second);
-            }
-            case "-" -> {
+
+            case "-":
                 return converter.convertToQuaternary(first - second);
-            }
-            case "*" -> {
+
+            case "*":
                 return converter.convertToQuaternary(first * second);
-            }
-            case "/" -> {
+
+            case "/" :
                 return converter.convertToQuaternary(first / second);
-            }
-            case "^" -> {
+
+            case "^" :
                 return converter.convertToQuaternary((int) Math.pow(first, second));
-            }
-            case "\u221A" -> {
+
+            case "\u221A" :
                 double num = Math.sqrt(first);
                 if (num % 1 == 0) {
                     return converter.convertToQuaternary((int) Math.sqrt(first));
                 }
                 return "No sqrt";
-            }
-            default -> {
+
+            default :
                 return "";
             }
-        }
     }
 }
+
