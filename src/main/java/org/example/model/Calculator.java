f020f99262f77abeced44c6cd69bbffa0bdec488
davisknight7 <davis.knight@bsu.edu>
2023-09-07 15:48:20 -0400

    Clean up code

--- src/main/java/org/example/model/Calculator.java
+++ src/main/java/org/example/model/Calculator.java
@@ -3,26 +3,32 @@ package org.example.model;
 public class  Calculator {
     public String calculate(int first, int second, String operation){
         Converter converter = new Converter();
-        switch(operation) {
-            case "+":
+        switch (operation) {
+            case "+" -> {
                 return converter.convertToQuaternary(first + second);
-            case "-":
+            }
+            case "-" -> {
                 return converter.convertToQuaternary(first - second);
-            case "*":
+            }
+            case "*" -> {
                 return converter.convertToQuaternary(first * second);
-            case "/":
+            }
+            case "/" -> {
                 return converter.convertToQuaternary(first / second);
-            case "^":
-                return converter.convertToQuaternary((int)Math.pow(first, second));
-            case "\u221A":
+            }
+            case "^" -> {
+                return converter.convertToQuaternary((int) Math.pow(first, second));
+            }
+            case "\u221A" -> {
                 double num = Math.sqrt(first);
                 if (num % 1 == 0) {
-                    return converter.convertToQuaternary((int)Math.sqrt(first));
+                    return converter.convertToQuaternary((int) Math.sqrt(first));
                 }
-
                 return "No sqrt";
-            default:
+            }
+            default -> {
                 return "";
+            }
         }
     }
 }
